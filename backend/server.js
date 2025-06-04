68eacba2e016cbe0b52d255a146e3c8647cabe56
richardalbani <richardalbani2@gmail.com>
2025-06-04 11:36:53 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static('frontend'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/frontend/index.html');
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+{
+  "name": "minhas-compras-backend",
+  "version": "1.0.0",
+  "description": "Backend simples para app Minhas Compras",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2"
+  }
+}
